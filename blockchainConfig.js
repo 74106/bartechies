a9c5ebc74d0ee68173f88fe1b3de54819cd55fc5
Daman Kumar Das <97243730+74106@users.noreply.github.com>
2026-03-14 11:43:29 +0530

    Delete blockchainConfig.js

--- blockchainConfig.js
+++
@@ -1,8 +0,0 @@
-(function (global) {
-  global.BLOCKCHAIN_CONFIG = {
-    CONTRACT_ADDRESS: '',
-    CHAIN_ID: 80002,
-    RPC_URL: 'https://rpc-amoy.polygon.technology',
-    CHAIN_NAME: 'Polygon Amoy Testnet',
-  };
-})(typeof window !== 'undefined' ? window : this);
